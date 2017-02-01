c5b1351e3ab287525e6e3013184576c712c808f3
Pavel <mobelus@mail.ru>
2017-02-01 18:48:28 +0300

    Update CannyStill.cpp

--- OpenCvX64ConsApp/OpenCvX64ConsApp/CannyStill.cpp
+++ OpenCvX64ConsApp/OpenCvX64ConsApp/CannyStill.cpp
@@ -34,42 +34,171 @@ Also when running your application use Ctrl + F5 to build and run it, this keeps
 #include<iostream>
 #include<conio.h>           // may have to modify this line if not using Windows
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-int main() {
+//using namespace cv;
+//using namespace std;
 
-	cv::Mat imgOriginal;        // input image
+
+static void help()
+{
+	std::cout << "\nThis program demonstrates circle finding with the Hough transform.\n"
+		"Usage:\n"
+		"./houghcircles <image_name>, Default is ../data/board.jpg\n" << std::endl;
+}
+
+#if 0
+int main(int argc, char** argv)
+{
+	/*
+	cv::CommandLineParser parser(argc, argv, "{help h ||}{@image|../data/board.jpg|}");
+	
+	if (parser.has("help"))
+	{
+		help();
+		return 0;
+	}
+	*/
+
+}
+#endif
+
+bool cvExample_SearchCircles(cv::Mat imgOriginalCopy, cv::Mat &imgFinalResult)
+{
+	cv::Mat img = imgOriginalCopy;
+	if (imgOriginalCopy.empty())
+	{
+		return false;
+	}
+
+	cv::Mat cimg;
+	medianBlur(img, imgOriginalCopy, 5);
+	cvtColor(imgOriginalCopy, cimg, cv::COLOR_GRAY2BGR);
+//	cvtColor(imgOriginalCopy, imgFinalResult, cv::COLOR_GRAY2BGR);
+	std::vector<cv::Vec3f> circles;
+	HoughCircles(imgOriginalCopy, circles, cv::HOUGH_GRADIENT, 1, 10,
+		100, 30, 1, 30 // change the last two parameters
+		// (min_radius & max_radius) to detect larger circles
+		);
+
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		cv::Vec3i c = circles[i];
+		circle(imgFinalResult, cv::Point(c[0], c[1]), c[2], cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
+		circle(imgFinalResult, cv::Point(c[0], c[1]), 2, cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+	}
+
+
+	/*
+	cv::Mat img = imgOriginalCopy;
+	if (img.empty())
+	{return false;}
+
+	cv::Mat cimg;
+	medianBlur(img, img, 5);
+	cvtColor(img, cimg, cv::COLOR_GRAY2BGR);
+	std::vector<cv::Vec3f> circles;
+	HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1, 10,
+		100, 30, 1, 30 // change the last two parameters
+		// (min_radius & max_radius) to detect larger circles
+		);
+
+	for (size_t i = 0; i < circles.size(); i++)
+	{
+		cv::Vec3i c = circles[i];
+		circle(cimg, cv::Point(c[0], c[1]), c[2], cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
+		circle(cimg, cv::Point(c[0], c[1]), 2, cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+	}
+	*/
+
+	//imshow("detected circles", cimg); cv::waitKey();
+
+	//imgFinalResult = cimg;
+	cv::namedWindow("imgCircles", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+	cv::imshow("imgCircles", imgFinalResult); cv::waitKey(0);
+}
+
+
+
+bool cvExample_ShowSomeFunctions(cv::Mat imgOriginalCopy, cv::Mat &imgFinalResult)
+{
 	cv::Mat imgGrayscale;       // grayscale of input image
 	cv::Mat imgBlurred;         // intermediate blured image
 	cv::Mat imgCanny;           // Canny edge image
 
-	imgOriginal = cv::imread("image.jpg");          // open image
+	if (imgOriginalCopy.empty()) {                                  // if unable to open image
+		std::cout << "error: image not read from file\n\n";     // show error message on command line
+		_getch();                                               // may have to modify this line if not using Windows
+		return false;                                              // and exit program
+	}
+
+	cv::cvtColor(imgOriginalCopy, imgGrayscale, CV_BGR2GRAY);       // convert to grayscale
+
+	cv::GaussianBlur(imgGrayscale,  // input image
+		imgBlurred,                 // output image
+		cv::Size(5, 5),             // smoothing window width and height in pixels
+		1.5);                       // sigma value, determines how much the image will be blurred
+
+	cv::Canny(imgBlurred,           // input image
+		//imgCanny,                   // output image
+		imgFinalResult,
+		100,                        // low threshold
+		200);                       // high threshold
+
+	// declare windows
+	// CV_WINDOW_AUTOSIZE is the default
+	cv::namedWindow("imgOriginalCopy", CV_WINDOW_AUTOSIZE);     // note: you can use CV_WINDOW_NORMAL which allows resizing the window
+	cv::imshow("imgOriginalCopy", imgOriginalCopy); cv::waitKey(0); // show window, until user presses a key
+
+	cv::namedWindow("imgGrayscale", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+	cv::imshow("imgGrayscale", imgGrayscale); cv::waitKey(0);
+
+	cv::namedWindow("imgBlurred", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+	cv::imshow("imgBlurred", imgBlurred); cv::waitKey(0);
+
+	cv::namedWindow("imgCanny", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+	//cv::imshow("imgCanny", imgCanny); cv::waitKey(0);
+	cv::imshow("imgCanny", imgFinalResult); cv::waitKey(0);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+int main() {
+
+	cv::Mat imgOriginal;  // input  image
+	cv::Mat imgOriginalCopy;  // input  image
+	cv::Mat imgFinal;     // output image
+
+//	imgOriginal = cv::imread("img.jpg");          // open image
+//	imgOriginal = cv::imread("..\\..\\pic\\pic.jpg");          // open image
+//	imgOriginal = cv::imread("..\\..\\..\\pic\\pic.jpg");          // open image
+	imgOriginal = cv::imread("C:\\_WORK_RGS\\MyFiles\\DISSER\\ISTRATOV\\videos\\ConsoleApplicationOpenCV\\pic\\pic.jpg");          // open image
 
 	if (imgOriginal.empty()) {                                  // if unable to open image
 		std::cout << "error: image not read from file\n\n";     // show error message on command line
 		_getch();                                               // may have to modify this line if not using Windows
 		return(0);                                              // and exit program
 	}
+	else
+	{
+		imgOriginalCopy = imgOriginal;
+	}
 
-	cv::cvtColor(imgOriginal, imgGrayscale, CV_BGR2GRAY);       // convert to grayscale
 
-	cv::GaussianBlur(imgGrayscale,          // input image
-		imgBlurred,                         // output image
-		cv::Size(5, 5),                     // smoothing window width and height in pixels
-		1.5);                               // sigma value, determines how much the image will be blurred
 
-	cv::Canny(imgBlurred,           // input image
-		imgCanny,                   // output image
-		100,                        // low threshold
-		200);                       // high threshold
+	// Exaple Function we will need:
+	//cvExample_ShowSomeFunctions(imgOriginal, imgFinal);
+
+	//imgOriginal = imgOriginalCopy;
+	cvExample_SearchCircles(imgOriginal, imgFinal);
+
 
 	// declare windows
-	cv::namedWindow("imgOriginal", CV_WINDOW_AUTOSIZE);     // note: you can use CV_WINDOW_NORMAL which allows resizing the window
-	cv::namedWindow("imgCanny", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
 	// CV_WINDOW_AUTOSIZE is the default
-	cv::imshow("imgOriginal", imgOriginal);     // show windows
-	cv::imshow("imgCanny", imgCanny);
+	cv::namedWindow("imgOriginal", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+	cv::imshow("imgOriginal", imgOriginal); cv::waitKey(0);
 
+	cv::namedWindow("imgFinal", CV_WINDOW_AUTOSIZE);        // or CV_WINDOW_AUTOSIZE for a fixed size window matching the resolution of the image
+	cv::imshow("imgFinal", imgFinal); cv::waitKey(0);
+	
 	cv::waitKey(0);                 // hold windows open until user presses a key
-
 	return(0);
 }
